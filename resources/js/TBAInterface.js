24359aaebcc89f180c04bd6f8881dc643c8ee1f3
Abrian Abir <32369387+a-abir@users.noreply.github.com>
2023-03-26 01:33:21 -0400

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "V5PkCoY9YEciBF17fpEkwAAco601QQP72A8G891hDbnnCuwYUdabd4SMIc7ACAgW";
 /**
  * Get list of teams in event
  *

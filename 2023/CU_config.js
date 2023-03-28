b14ebfbb47248d0814ab95825dd89bf4f8d5c565
Abrian Abir <32369387+a-abir@users.noreply.github.com>
2023-03-28 19:14:11 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023flor",
+      "defaultValue": "2023tnkn",
       "required": "true",
       "disabled": "true"
     },

1a937d27526d758ded21cbf163c5ba60377fdf7d
adam mcdaniel <adam.mcdaniel17@gmail.com>
2023-04-06 08:45:16 -0400

    Actualizar CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023tnkn",
+      "defaultValue": "2023alhu",
       "required": "true",
       "disabled": "true"
     },
